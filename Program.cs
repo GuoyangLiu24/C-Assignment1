719f3c85aa1860562e4bc22be13ebe5bff5e7596
GuoyangLiu24 <liuguoyang24@gmail.com>
2020-06-08 00:08:23 -0400

    C# Assignment1 Backup

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guoyang_Liu_Sec003_Lab01
+{
+    public delegate string SmallestString(string playerName);
+    class Program
+    {
+           
+        static void Main(string[] args)
+        {
+            // create an instance of the delegate 
+            Func<string, string, string, string> func = Minimum;
+            // call Minimum using 3 delegate variables
+            Console.WriteLine($"The smallest string is {func("Giannis Antetokounmpo", "Kawhi Leonard", "Ming Yao")}");
+
+            // create an instance of the delegate 
+            Action<double, double, double> action = AvgGrade;
+            // call AvgGrade using 3 delegate variables
+            action(99.99, 88.88, 77.77);
+
+
+        } // end main method
+
+        private static string Minimum(string string1, string string2, string string3)
+        {
+            string result = string1;
+            if (string.Compare(string1, string2, true) == 1)
+            {
+                result = string2;
+            }
+
+            else if (string.Compare(result, string3, true) == 1)
+            {
+                result = string3;
+
+            }
+            return result;
+        }//end the filterString method
+
+        private static void AvgGrade(double value1, double value2, double value3)
+        {
+            double result = (value1 + value2 + value3) / 3;
+            Console.WriteLine($"The average of grades is {result}");
+
+        }// end AvgGrade method
+
+
+    }// end class
+
+}// end namespace
+            
+    
+            
+          
+    
+           
+         
+           
+
+        
+
+
+    
+
+       
+
+
+
---
+++ guoyang_liu_exercise2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guoyang_liu_exercise2
+{
+    class Program
+    {
+        // delegate for a function that receives an int and returns a bool
+        public delegate bool GradesPredicate(double grade);
+
+        static void Main(string[] args)
+        {
+            double[] grades = { 11.11, 22.22, 33.33, 44.44, 55.55, 66.66, 77.77, 88.88, 99.99, 50.00 };
+
+            GradesPredicate fiftyPredicate =  grade => grade >= 50;
+
+            GradesFilter(grades, fiftyPredicate );
+        }
+
+        private static void GradesFilter(double[] doubleArray, GradesPredicate predicate)
+        {
+            // hold the selected elements
+            var result = new List<double>();
+
+            // iterate over each element in the array
+            foreach (var item in doubleArray)
+            {
+                // if the element satisfies the predicate
+                if (predicate(item))
+                {
+                    result.Add(item); // add the element to the result
+                }
+            }
+
+            Console.WriteLine($"Here are those grades values which are greater than or equal to 50:");
+            // iterate over each element in the List
+            foreach (double item in result)
+            {
+                Console.WriteLine($"{item:f2} "); // print item followed by a space
+            }
+
+            Console.WriteLine(); // add a new line
+
+
+        }
+
+    }
+}
